090516eefae3943a8d63c4867f2ac23af76e4c71
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-01-17 19:26:37 +0530

    Time: 564 ms (41.85%), Space: 180.5 MB (41.39%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        map<int,int>looser;
-        set<int> ws;
-        vector<int>l;
-        vector<int>w;
-        for(auto arr:matches){
-               looser[arr[1]]++;
+           map<int,int>winner;
+           map<int,int>looser;
+           vector<int>w;
+           vector<int>l;
+           set<int>ws;
+         for(int i=0;i<matches.size();i++){
+                for(int j=0;j<matches[0].size();j++){
+                       if(j==0) winner[matches[i][j]]++;
+                       else looser[matches[i][j]]++;
+                }
+         }
+        for( auto it:winner){
+               if(looser.find(it.first)==looser.end())  ws.insert(it.first);
+              
         }
-        for(auto arr:matches){
-               if(looser.find(arr[0])==looser.end()) ws.insert(arr[0]);
-        }
-        for(auto &[f,s]:looser){
-               if(s==1) l.push_back(f);
+        for(auto it:looser){
+               if(it.second==1) l.push_back(it.first);
         }
         for(auto it:ws){
-              w.push_back(it);
+             w.push_back(it);
         }
         vector<vector<int>>res;
         res.push_back(w);
         res.push_back(l);
-        return res;
+    return res;
     }
 };
