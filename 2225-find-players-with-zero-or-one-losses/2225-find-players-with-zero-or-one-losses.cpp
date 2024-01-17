283e870dc2e4a0a26d101788bcad7003608c3b49
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-01-17 19:38:39 +0530

    Time: 587 ms (35.18%), Space: 165 MB (67.66%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-           map<int,int>winner;
            map<int,int>looser;
+           set<int>ws;
            vector<int>w;
            vector<int>l;
-           set<int>ws;
-         for(int i=0;i<matches.size();i++){
-                for(int j=0;j<matches[0].size();j++){
-                       if(j==0) winner[matches[i][j]]++;
-                       else looser[matches[i][j]]++;
-                }
-         }
-        for( auto it:winner){
-               if(looser.find(it.first)==looser.end())  ws.insert(it.first);
-              
-        }
-        for(auto it:looser){
-               if(it.second==1) l.push_back(it.first);
-        }
-        for(auto it:ws){
-             w.push_back(it);
-        }
-        vector<vector<int>>res;
-        res.push_back(w);
-        res.push_back(l);
+           for(int i=0;i<matches.size();i++){
+                  for(int j=0;j<matches[0].size();j++){
+                          if(j==1) looser[matches[i][j]]++;
+                  }
+           }
+          for(int i=0;i<matches.size();i++){
+                  for(int j=0;j<matches[0].size();j++){
+                          if(j==0){
+                                 if(looser.find(matches[i][j])==looser.end()) ws.insert(matches[i][j]);
+                          }
+                  }
+           }
+          for(auto &[f,s]:looser){
+                  if(s==1) l.push_back(f);
+          }
+          for(auto it:ws){
+                w.push_back(it);
+          }
+          vector<vector<int>>res;
+          res.push_back(w);
+          res.push_back(l);
     return res;
     }
 };
