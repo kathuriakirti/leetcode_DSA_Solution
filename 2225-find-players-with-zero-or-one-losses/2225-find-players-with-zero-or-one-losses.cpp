7b00ac6899471debe7f1126dac777922cfe2cd08
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-01-17 17:07:13 +0530

    Time: 582 ms (36.70%), Space: 165 MB (66.85%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        map<int,int>winner;
         map<int,int>looser;
-        set<int> keysToRemove;
+        set<int> ws;
         vector<int>l;
         vector<int>w;
         for(int i=0;i<matches.size();i++){
               for(int j=0;j<matches[0].size();j++){
-                     if(j==0) winner[matches[i][j]]++;
-                     else looser[matches[i][j]]++;
+                     if(j==1) looser[matches[i][j]]++;   
               }
         }
-        for (auto it : winner) {
-            if(looser.find(it.first) == looser.end()) {
-                keysToRemove.insert(it.first);
-            } 
+        for(int i=0;i<matches.size();i++){
+                for(int j=0;j<matches[0].size();j++){
+                        if(j==0){
+                               if(looser.find(matches[i][j])==looser.end()){
+                                       ws.insert(matches[i][j]);
+                               }
+                        }
+                }
         }
-        for (auto it : looser) {
-            if(it.second==1) l.push_back(it.first);
+        for(auto &[f,s]:looser){
+               if(s==1) l.push_back(f);
         }
-         for (auto it : keysToRemove) {
-             w.push_back(it);
+        for(auto it:ws){
+              w.push_back(it);
         }
         vector<vector<int>>res;
         res.push_back(w);
