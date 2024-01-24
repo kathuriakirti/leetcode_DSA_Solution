711d244470cfafb808a1599eb2f8a113b7535efb
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-01-24 17:24:20 +0530

    Time: 67 ms (98.42%), Space: 62.1 MB (6.43%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,12 +5,9 @@ public:
           while(start<=end){
                 int mid= start+(end-start)/2;
                 if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
-                else if(arr[mid]>arr[mid+1]) end=mid;
+                else if(arr[mid]>arr[mid+1]) end=mid; 
                 else if(arr[mid]<arr[mid+1]) start=mid+1;  
           }
         return -1;
-// //         [18,29,38,59,98,100,99,98,90]
-//          start=5, end=5
-//         mid= 6
     }
 };
