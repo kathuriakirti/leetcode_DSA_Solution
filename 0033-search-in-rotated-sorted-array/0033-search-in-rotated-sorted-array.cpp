0396526331cbd221ab8cb1d5bd25ef0c279af0f7
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-01-28 00:10:16 +0530

    Time: 7 ms (22.89%), Space: 13.8 MB (9.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,50 @@
 class Solution {
 public:
-   int linear_search(vector<int>& nums, int target){
-           for(int i=0;i<nums.size();i++){
-                  if(nums[i]==target) return i;
-           }
-       return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int ans=linear_search(nums,target);
-        return ans;
-    
-    }
+     int FindPivotIndex(vector<int>num, int target) {
+         int left = 0, right = num.size() - 1;
+         int n=num.size();
+         while (left <= right) {
+             int mid = left + (right - left) / 2;
+             if(left==right) return left;
+             if (mid-1>=0 && num[mid] < num[mid - 1]) {
+                 return mid - 1;
+             } else if (mid+1<n && num[mid] > num[mid + 1]) {
+                 return mid;
+             } else if (num[mid]<num[left]) {
+                 right = mid - 1;
+             } else {
+                 left = mid + 1;
+             }
+         }
+         return -1;
+     }
+     int search_element(vector<int>num, int target, int left, int right) {
+         while (left <= right) {
+             int mid = left + (right - left) / 2;
+             if (num[mid] == target) {
+                 return mid;
+             } else if (num[mid] < target) {
+                 left = mid + 1;
+             } else {
+                 right = mid - 1;
+             }
+         }
+         return -1;
+     }
+     int search(vector<int>& num, int target) {
+         int pivotIndex = FindPivotIndex(num, target);
+         int n = num.size();
+         int ans = -1;
+         if (n == 1) {
+             if (target == num[0]) {
+                 ans = 0;
+             }
+         } 
+         if (target >= num[0] && target <= num[pivotIndex]) {
+             ans = search_element(num, target, 0, pivotIndex);
+         } else {
+             ans = search_element(num, target, pivotIndex + 1, n - 1);
+         }
+         return ans;
+     }
 };
