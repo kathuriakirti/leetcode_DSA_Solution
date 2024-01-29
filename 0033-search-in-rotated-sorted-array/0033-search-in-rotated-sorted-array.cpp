9caba631048b94465b6d6dd481eabf90f197dee0
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-01-29 17:15:00 +0530

    Time: 2 ms (70.25%), Space: 13.5 MB (12.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,54 +1,47 @@
-class Solution {
+ class Solution {
 public:
-     int FindPivotIndex(vector<int>num, int target) {
-         int left = 0, right = num.size() - 1;
-         int n=num.size();
-         while (left <= right) {
-             int mid = left + (right - left) / 2;
-             if(left==right) return left;
-             if (mid-1>=0 && num[mid] < num[mid - 1]) {
-                 return mid - 1;
-             } else if (mid+1<n && num[mid] > num[mid + 1]) {
-                 return mid;
-             } else if (num[mid]<num[0]) {
-                 right = mid - 1;
-             } else {
-                 left = mid + 1;
-             }
-         }
-         return -1;
-     }
-     int search_element(vector<int>num, int target, int left, int right) {
-         while (left <= right) {
-             int mid = left + (right - left) / 2;
-             if (num[mid] == target) {
-                 return mid;
-             } else if (num[mid] < target) {
-                 left = mid + 1;
-             } else {
-                 right = mid - 1;
-             }
-         }
-         return -1;
-     }
-     int search(vector<int>& num, int target) {
-         int pivotIndex = FindPivotIndex(num, target);
-         int n = num.size();
-         int ans = -1;
-         // if (n == 1) {
-         //     if (target == num[0]) {
-         //         ans=0;
-         //     }
-         // } 
-         // else if(n==2){
-         //      if(target==num[0]) ans=0;
-         //      else if(target==num[1]) ans=1;
-         // }
-          if (target >= num[0] && target <= num[pivotIndex]) {
-             ans = search_element(num, target, 0, pivotIndex);
-         } else {
-             ans = search_element(num, target, pivotIndex + 1, n - 1);
-         }
-         return ans;
-     }
+    int FindPivotIndex(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+    
+    int binarySearch(vector<int>& nums, int target, int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        if (n == 0) return -1;
+        
+        int pivotIndex = FindPivotIndex(nums);
+        
+        if (pivotIndex == 0 || target < nums[0]) {
+            return binarySearch(nums, target, pivotIndex, n - 1);
+        } else {
+            return binarySearch(nums, target, 0, pivotIndex - 1);
+        }
+    }
 };
