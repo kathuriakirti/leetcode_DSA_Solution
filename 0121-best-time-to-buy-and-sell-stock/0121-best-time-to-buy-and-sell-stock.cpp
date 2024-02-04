353b1308a6c7f396ad0a12359a0de8bcd2923763
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-04 11:32:54 +0530

    Time: 88 ms (78.12%), Space: 95.9 MB (12.70%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,42 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         int n=prices.size();
-        int min=INT_MAX,max=INT_MIN;
-        int ans=0;
-        int buy_day=1;
-         bool min_found=false;
-        int sell_day;
-         for(int i=0;i<n;i++){
-                  if(min>prices[i]){
-                       min=prices[i];
-                       min_found=true;
-                       buy_day=i;
-                      sell_day=buy_day+1;
-                  }
-              
-              if(min_found==true && sell_day<n){
-                    
-                     if(prices[buy_day]<prices[sell_day]){
-                           int profit=  prices[sell_day]-prices[buy_day];
-                           if(ans<profit) ans=profit;
-                     }
-                  sell_day++;
-              }
-         }
-//          if(min_found=true && buy_day+1<n ){
-//                 for(int i=0;i<n;i++){
-//                   if(max<prices[i]){
-//                        max=prices[i];
-//                         profit=max-min;
-//                   }
-                    
-//                 }
-                  
-//          }
-//         cout<<profit<<endl;
-         
-        if(ans) return ans;
-        else return 0;
+        int n= prices.size();
+        int buy_price=INT_MAX,sell_price=INT_MIN;
+        for(int i=0;i<n;i++){
+              buy_price=min(buy_price,prices[i]);
+              sell_price=max(sell_price,prices[i]-buy_price);
+        }
+        return sell_price;
     }
 };
