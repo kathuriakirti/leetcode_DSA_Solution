32ba6aa355b2e710e573ec8a6a0efc2e3cbc781f
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-07 14:07:39 +0530

    Time: 3 ms (91.97%), Space: 15.9 MB (28.98%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int first_idx(vector<int>& arr, int target){
-           int first_idx=-1;
-           int n= arr.size()-1;
-           int start=0,end=n;
-            while(start<=end){
-                int mid = start+(end-start)/2;
-                if(arr[mid]==target){
-                     first_idx=mid;
-                     end=mid-1;
-                }
-               else if(arr[mid]<target) start=mid+1;
-               else end=mid-1;
-           }
-     return first_idx;
-    }
-     int last_idx(vector<int>& arr, int target){
-           int last_idx=-1;
-           int n= arr.size()-1;
-           int start=0,end=n;
-            while(start<=end){
-                int mid = start+(end-start)/2;
-                if(arr[mid]==target){
-                     last_idx=mid;
-                     start=mid+1;
-                }
-               else if(arr[mid]<target) start=mid+1;
-               else end=mid-1;
-           }
-     return last_idx;
+    vector<int>search(vector<int>&nums,int target){
+          int n=nums.size();
+          int st=0,e=n-1;
+          int first_idx=-1,last_idx=-1;
+          while(st<=e){
+                  int mid= st+(e-st)/2;
+                  if(nums[mid]==target){
+                       first_idx=mid;
+                        e--;
+                  }
+                  else if(nums[mid]<target) st++;
+                  else e--;
+          }
+          st=0,e=n-1;
+           while(st<=e){
+                  int mid= st+(e-st)/2;
+                  if(nums[mid]==target){
+                       last_idx=mid;
+                        st++;
+                  }
+                  else if(nums[mid]<target) st++;
+                  else e--;
+          }
+     return {first_idx,last_idx};
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-         int  first_index,last_index;
-         first_index= first_idx(nums,target);
-         last_index = last_idx(nums,target);
-        return {first_index,last_index};
-        
+          vector<int>ans=search(nums,target);
+          return ans;
     }
 };
