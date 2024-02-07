683304f3ffc5f591f952d8b9dbe1b5c2701abd80
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-07 11:06:43 +0530

    Time: 21 ms (67.28%), Space: 27.3 MB (18.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,51 +1,18 @@
 class Solution {
 public:
-    void temp_arr(vector<int>& nums, int k){
-           int n= nums.size();
-            vector<int>temp_array(k);
-           for(int i=0;i<k;i++){
-                   temp_array[i]=nums[n-i-1];
-           }
-         reverse(temp_array.begin(),temp_array.end());
-         for(int i=n-1;i>=k;i--){
-               nums[i]=nums[i-k];
-         }
-         for(int i=0;i<k;i++){
-               nums[i]=temp_array[i];
-         }
-    }
-    void reverse_method(vector<int>& nums, int k){
-          int n=nums.size();
-           k=k%n;
-          int left=0,right=nums.size()-1;
-          while(left<right){
-                 swap(nums[left],nums[right]);
-                 left++; right--;
+    void array_reverse(vector<int>&nums,int st,int e){
+          while(st<e){
+                swap(nums[st],nums[e]);
+                st++;e--;
           }
-         left=0,right=k-1;
-        while(left<right){
-                swap(nums[left],nums[right]);
-                 left++; right--;
-        }
-        left=k, right=n-1;
-        while(left<right){
-               swap(nums[left],nums[right]);
-               left++; right--;
-        }
-    }
-    void modulus(vector<int>& nums, int k){
-           int n= nums.size();
-           vector<int>ans(n);
-           for(int index=0;index<n;index++){
-                  int new_index=  (index+k)%n;
-                  ans[new_index]=nums[index];
-           }
-         nums=ans;
     }
     void rotate(vector<int>& nums, int k) {
-          // k=k%nums.size();
-          // temp_arr(nums,k);
-          // modulus(nums,k);
-          reverse_method(nums,k);
+       
+        int n=nums.size();
+         k=k%n;
+        array_reverse(nums,0,n-1);
+        array_reverse(nums,0,k-1);
+        array_reverse(nums,k,n-1);
+        
     }
 };
