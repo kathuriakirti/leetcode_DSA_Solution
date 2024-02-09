4c8ed0cb51260cb44508c081519f4a6d708bd9b8
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-09 18:53:39 +0530

    Time: 16 ms (72.87%), Space: 34 MB (22.99%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,52 +1,13 @@
 class Solution {
 public:
-    int bruteforce(vector<int>&nums){
-            int n= nums.size();
-            for(int i=0;i<n;i++){
-                    int lsum=0,rsum=0;
-                    for(int j=0;j<i;j++){
-                            lsum+=nums[j];
-                    }
-                    for(int k=i+1;k<n;k++){
-                            rsum+=nums[k];
-                    }
-                    if(lsum==rsum) return i;
-            }
-    return -1;
-    }
-    int better_sol(vector<int>nums){
-             int n=nums.size();
-             vector<int>lsum(n,0);
-             vector<int>rsum(n,0);
-             for(int i=1;i<n;i++){
-                   lsum[i]= lsum[i-1]+nums[i-1];
-             }
-             for(int j=n-2;j>=0;j--){
-                   rsum[j]= rsum[j+1]+nums[j+1];
-             }
-             for(int i=0;i<n;i++){
-                    if(lsum[i]==rsum[i]) return i;
-             }
-    return -1;
-    }
-    int optimal_sol(vector<int>nums){
-             int n=nums.size();
-             int total=0;
-             for(int i=0;i<n;i++){
-                   total+=nums[i];
-             }
-             int lsum=0;
-             for(int i=0;i<n;i++){
-                   int rsum=total-lsum-nums[i];
-                   if(rsum==lsum) return i;
-                   lsum+=nums[i];
-             }
-    return -1;
-         
-    }
     int pivotIndex(vector<int>& nums) {
-        //  return bruteforce(nums); 
-        // return better_sol(nums);
-        return optimal_sol(nums);
+          int totalSum=0;
+          for(auto num:nums) totalSum+=num;
+          int currSum=0;
+          for(int i=0;i<nums.size();i++){
+                if(totalSum-currSum-nums[i]==currSum) return i;
+                currSum+=nums[i];
+          }
+    return -1;
     }
 };
