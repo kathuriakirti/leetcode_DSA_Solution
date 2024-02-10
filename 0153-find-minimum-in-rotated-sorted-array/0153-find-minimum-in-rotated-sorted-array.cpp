8177fd2725b8150ad387a44882f71d4527d1acce
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-10 09:56:52 +0530

    Time: 0 ms (100.00%), Space: 12.6 MB (22.18%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    int findPivotIdx(vector<int>&arr){
-         int n=arr.size();
-         int st=0,e=n-1;
-         while(st<=e){
-               int mid= st+(e-st)/2;
-               if(st==e) return st;
-               else if(mid+1<n && arr[mid]>arr[mid+1]) return mid;
-               else if(mid-1>=0 && arr[mid-1]>arr[mid]) return mid-1;
-               else if(arr[mid]<=arr[0]) e=mid;
-               else st=mid+1;
-              cout<<"e: "<<e<<" ";
-         }
-    return -1;
-    }
     int findMin(vector<int>& nums) {
-         int n=nums.size();
-        
-         int pivotIdx= findPivotIdx(nums);
-         cout<<"\npivotIdx: "<<pivotIdx;
-         if(n==1) return nums[0];
-         else if(n==2) return min(nums[0],nums[1]);
-         else if(pivotIdx>=0 && pivotIdx<n-1) return nums[pivotIdx+1];
-         else return nums[0];
-    return -1;
-         
+          int n=nums.size();
+          int st=0,e=n-1;
+          while(st<e){
+                // if array is not rotated and containing test case like [1,3]
+                if(nums[st]<nums[e]) return nums[st];
+                int mid= st+(e-st)/2;
+                if(nums[mid]>=nums[st]) st=mid+1; //[5,4,3,2], [2,1]
+                else e=mid;
+          }
+        return nums[st];
     }
 };
