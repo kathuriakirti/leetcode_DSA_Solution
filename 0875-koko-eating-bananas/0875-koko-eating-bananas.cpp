449698056581b6191a640fde0969f6209021e822
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-16 21:12:25 +0530

    Time: 37 ms (49.72%), Space: 21.4 MB (18.14%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-   bool canEatBananas(vector<int>&piles,long long mid,int h){
+    bool minSpeed(vector<int>& piles, int h,long long mid){
          long long actual_hour=0;
-         for(int x:piles){
-              actual_hour+=x/mid;
-              if(x%mid!=0) actual_hour++;
+         for(auto x: piles){
+               actual_hour+=x/mid;
+               if(x%mid!=0) actual_hour++;
          }
         return actual_hour<=h;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1,high=*max_element(begin(piles),end(piles));
-        while(low<=high){
-            long long mid= low+(high-low)/2;
-            if(canEatBananas(piles,mid,h)) high=mid-1;
-            else low=mid+1;
-        }
+          int low=1,high=*max_element(piles.begin(),piles.end()),ans;
+          while(low<=high){
+               long long mid=(long long)low+(high-low)/2;
+               if(minSpeed(piles,h,mid)) high=mid-1;
+               else low=mid+1;
+          }
     return low;
     }
 };
