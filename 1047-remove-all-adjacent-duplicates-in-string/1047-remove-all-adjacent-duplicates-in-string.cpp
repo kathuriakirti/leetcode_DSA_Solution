338098a71482da533e4955dada216dc3ece54eb2
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-27 07:36:42 +0530

    Time: 21 ms (55.65%), Space: 12.1 MB (54.70%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-          string temp ="";
-          int n= s.length();
-          for(int i=0;i<n;i++){
-                if( temp.length()>0 && s[i]==temp[temp.length()-1]) temp.pop_back();
-                else temp.push_back(s[i]);
-          }
-        return temp;
+        string ans;
+        for(int i=0;i<s.size();i++){
+               if(ans.size()>0 && s[i]==ans[ans.size()-1])
+                       ans.pop_back();
+               else ans.push_back(s[i]);
+        }
+    return ans;
     }
 };
