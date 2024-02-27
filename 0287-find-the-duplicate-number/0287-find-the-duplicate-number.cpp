7e2c5c9154429306a7f3d34bbfe192c45c01ac76
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-27 20:35:10 +0530

    Time: 170 ms (15.91%), Space: 104.2 MB (5.69%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    int negative_marking(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            int index = abs(nums[i]);
-            if (nums[index] < 0)
-                return index;
-            else {
-                nums[index] *= -1;
-            }
-        }
-        return -1;
-    }
-    int swap_element(vector<int>& nums) {
-        while (nums[0] != nums[nums[0]]) {
-            swap(nums[0], nums[nums[0]]);
-        }
-        return nums[0];
-    }
     int findDuplicate(vector<int>& nums) {
-        return negative_marking(nums);
-        // swap elements to its associated index
-        // return swap_element(nums);
+          unordered_map<int,int>umap;
+          for(int i=0;i<nums.size();i++){
+                 umap[nums[i]]++;
+          }
+         for(auto x:umap){
+               if(x.second>1) return x.first;
+         }
+    return -1;
     }
 };
