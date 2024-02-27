321db229d42bebdbabf5f152ffaa616264b6ac10
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-27 16:12:29 +0530

    Time: 13 ms (86.64%), Space: 12.1 MB (56.66%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    bool checkLastKChar( string &ans, int k,char ch){
-            int it= ans.size()-1;
-           for(int i=0;i<k-1;i++){
-                  if(ans[it]!=ch) return false;
-                  it--;
-           }
-    return true;
-    }
     string removeDuplicates(string s, int k) {
-         string ans;
-         for(int i=0;i<s.size();i++){
-                if(ans.size()<k-1) ans.push_back(s[i]);
-                else{
-                      if(checkLastKChar(ans,k,s[i])){
-                             for(int i=0;i<k-1;i++) ans.pop_back();
-                      }
-                      else ans.push_back(s[i]);
-                }
-                    
-         }
-      return ans;
+          vector<int>count(s.size());
+          int i=0,j=0;
+          while(j<s.size()){
+                 s[i]=s[j];
+                 count[i]=1;
+                 if(i>0 && s[i]==s[i-1]) count[i]+=count[i-1];
+                 if(count[i]==k) i-=k;
+                 ++i;++j;
+              
+          }
+    return s.substr(0,i);
     }
-
 };
