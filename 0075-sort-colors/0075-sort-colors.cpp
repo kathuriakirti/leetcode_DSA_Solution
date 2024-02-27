fc144213829f13889e80914f81ea44bcfa27896a
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-27 19:48:57 +0530

    Time: 5 ms (15.24%), Space: 9.8 MB (21.50%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
-    void count(vector<int>nums){
-         int zeros=0,ones=0,twos=0;
-         for(int i=0;i<nums.size();i++){
-               if(nums[i]==0) zeros++;
-               else if(nums[i]==1) ones++;
-               else twos++;
-         }
-         int i=0;
-         while(zeros--){
-             nums[i]=0;
-             i++;
-         }
-         while(ones--){
-             nums[i]=1;
-             i++;
-         }
-         while(twos--){
-             nums[i]=2;
-             i++;
-         }
-    }
     void sortColors(vector<int>& nums) {
-        //  count(nums);
-         int idx=0;
-         int l=0,h=nums.size()-1;
-         while(idx<=h){
-                if(nums[idx]==0){
-                      swap(nums[l],nums[idx]);
-                      l++;
-                      idx++;
-                }
-                else if(nums[idx]==1){
-                       idx++;
-                }
-                else{ // nums[idx]==2
-                       swap(nums[h],nums[idx]);
-                       h--;
-                }
+         int i=0,j=nums.size()-1,it=0;
+        while(it<=j){
+                 if(nums[it]==0){
+                        swap(nums[it],nums[i]);
+                        it++; i++;
+                 }
+                 else if(nums[it]==2){
+                       swap(nums[it],nums[j]);
+                       j--;
+                 }
+                 else it++;
+               cout<<it<<" ";
          }
     }
 };
