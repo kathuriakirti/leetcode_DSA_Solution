d4714b25c90841e80c8aaa7ee5cff2836e8d77db
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-02-29 12:02:22 +0530

    Time: 13 ms (86.46%), Space: 12.1 MB (58.10%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-   bool check(string &ans, int k,char curr_element){
-          int it= ans.size()-1;
-          for(int i=0;i<k-1;i++){
-                if(ans[it]!=curr_element) return false;
-                it--;
-          }
-    return true;
-    }
     string removeDuplicates(string s, int k) {
-          string ans;
-          for(int i=0;i<s.length();i++){
-                  if(ans.size()<k-1) ans.push_back(s[i]);
-                  else{ 
-                         // check last k-1 characters
-                         if(check(ans,k,s[i])){
-                                for(int i=0;i<k-1;i++){
-                                        ans.pop_back();
-                                }
-                               // cout<<ans<<endl;
-                         }
-                         else ans.push_back(s[i]);
-                  }
+          vector<int>count(s.size());
+          int i=0,j=0;
+          for(int it=0;it<s.size();it++){
+                 s[i]=s[j];
+                 count[i]=1;
+                 if(i>0 && s[i]==s[i-1]) count[i]+=count[i-1];
+                 if(count[i]==k) i-=k;
+                 i++; j++;
           }
-     return ans;
+     return s.substr(0,i);
     }
+   
 };
