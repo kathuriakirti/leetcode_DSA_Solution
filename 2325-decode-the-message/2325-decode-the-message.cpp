6eb62be7eb2455d3305cab1fc5c66bc57ccd135b
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-03-04 16:36:13 +0530

    Time: 5 ms (43.23%), Space: 8 MB (70.46%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.cpp
+++ 2325-decode-the-message/2325-decode-the-message.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string decodeMessage(string key, string message) {
-          char mapping[257] = {0};
-          char start='a';
-          for(auto ch : key){
-                if(ch!=' ' && mapping[ch]==0 ){  
-                       mapping[ch]= start;
-                        start++;
-                }
+           vector<char>mapping(256,0);
+           char start='a';
+           for(int i=0;i<key.length();i++){
+                    char ch= key[i];
+                    if(ch!=' ' && mapping[ch]==0){
+                            mapping[ch]=start;
+                            start++;
+                    }
+           } 
+          for(int i=0;i<message.length();i++){
+                 char ch=message[i];
+                 if(ch!=' '){
+                        message[i]= mapping[ch];
+                 }
           }
-          string ans;
-          for(auto ch: message){
-                if(ch==' ') ans.push_back(' ');
-                else{
-                     ans.push_back(mapping[ch]);
-                }
-          }
-    return ans;
+    return message;
     }
 };
