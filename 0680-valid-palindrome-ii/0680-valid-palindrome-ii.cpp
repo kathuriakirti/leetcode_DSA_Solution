a4d09abfe1f287c2df6a45c1d04773f4d19696f8
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-03-04 16:58:49 +0530

    Time: 58 ms (36.00%), Space: 21 MB (82.36%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    bool checkPalindrome(string s, int i,int j){
-          while(i<j){
-                if(s[i]!=s[j]) return false;
-                else{
-                      i++; j--;
-                }
+    bool checkPalindrome(string &s, int low,int high){
+          while(low<high){
+                if(s[low]!=s[high]) return false;
+                low++; high--;
           }
     return true;
     }
     bool validPalindrome(string s) {
-          int st=0,e=s.length()-1;
-          while(st<e){
-               if(s[st]==s[e]){
-                     st++; e--;
-               }
-               else{
-                      // ith char remove
-                      bool ans1= checkPalindrome(s,st+1,e);
-                     // jth char remove
-                       bool ans2= checkPalindrome(s,st,e-1);
-                     return ans1||ans2;
-               }
-          }
+           int low=0,high=s.length()-1;
+           while(low<high){
+                   if(s[low]!=s[high]){
+                         bool ans1= checkPalindrome(s,low+1,high);
+                         bool ans2= checkPalindrome(s,low,high-1);
+                         return ans1 || ans2;
+                   }
+                  low++; high--;
+           }
     return true;
+      
     }
 };
