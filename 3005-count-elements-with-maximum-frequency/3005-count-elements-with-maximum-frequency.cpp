798b7a8b22754f7881cb901d5aec18ccd7699d74
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-03-08 22:15:52 +0530

    Time: 0 ms (100.00%), Space: 23.2 MB (5.04%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -5,11 +5,8 @@ public:
           int maxOccurence=0;
           for(auto num:nums){
                  umap[num]++;
+                 maxOccurence = max(maxOccurence,umap[num]);
           }
-          for(auto x:umap){
-                 maxOccurence = max(maxOccurence,x.second);
-          }
-         cout<<maxOccurence<<endl;
          int count=0;
          // in umap there will  be not duplicates key that's why will            find through vector
          for(auto [c1,c2]:umap){
