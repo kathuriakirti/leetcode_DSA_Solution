247eb22e6e8f9d0e7b50a98b6b7458b6bee90d52
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-06-12 19:33:40 +0530

    Time: 5 ms (54.00%), Space: 8.1 MB (84.46%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    string addRE(string num1,string num2,int i,int j,int carry=0){
+    void addRE(string &num1,string &num2,int i,int j,int carry,string &ans){
           // base condition
           // last carry add when both i and j less than 0 otherwise return empty string
           if(i<0 && j<0){
                 if(carry!=0){
-                       return string(1,carry+'0');
+                       ans.push_back(carry+'0');
                 }
-                return "";
-                }
-          
+                return;
+          }
         
          int Num1 = (i>=0?num1[i]:'0')-'0';
          int Num2 = (j>=0?num2[j]:'0')-'0';
          int add = Num1+Num2+carry;
          int digit = add%10;
           carry = add/10;
-         string ans = "";
          ans.push_back(digit+'0');
-         ans = addRE(num1,num2,i-1,j-1,carry)+ans ;
-         return ans;
+          addRE(num1,num2,i-1,j-1,carry,ans) ;
     }
     string addStrings(string num1, string num2) {
            int i=num1.size()-1,j= num2.size()-1;
-           string ans= addRE(num1,num2,i,j);
-            // reverse(ans.begin(),ans.end());
+           string ans ="";
+            addRE(num1,num2,i,j,0,ans);
+           reverse(ans.begin(),ans.end());
            return ans;
     }
 };
