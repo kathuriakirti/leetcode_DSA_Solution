bdcbe02ce105e342346cdf8484d1ae7dcecf252c
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-07-13 13:44:16 +0530

    Time: 99 ms (80.21%), Space: 71.4 MB (59.83%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,51 +1,45 @@
 class Solution {
 public:
-    void merge(vector<int>&ans,vector<int>&nums,int st, int e){
-            int mid = (st+e)/2;
-            // compare left subarrays with right subarrays
-            int k=st,leftIdx=st,rightIdx=mid+1;
-            while(leftIdx<=mid && rightIdx<=e){
-                   if(nums[leftIdx]<nums[rightIdx]){
-                         ans[k] = nums[leftIdx];
-                         leftIdx++;
-                   }
-                   else{
-                         ans[k] = nums[rightIdx];
-                         rightIdx++;
-                   }
-                  k++;
-            }
-           // left subarray not completed
-           while(leftIdx<=mid){
-                 ans[k] = nums[leftIdx];
-                  leftIdx++;
-                 k++;
-           }
-           // right subarray not completed
-           while(rightIdx<=e){
-                  ans[k] = nums[rightIdx];
-                  rightIdx++;
-                  k++;
-           }
-          for(int i=st;i<=e;i++){
-                nums[i] = ans[i];
+   void merge(vector<int>&nums, int st, int e,vector<int>&temp){
+          
+         
+          int mid= (st+e)/2;
+          int leftIdx =st, rightIdx=mid+1, mainIdx=st;
+          while(leftIdx<=mid && rightIdx<=e){
+                if(nums[leftIdx]<nums[rightIdx]) {
+                      temp[mainIdx] =nums[leftIdx++];
+                     
+                }
+                else{
+                       temp[mainIdx] =nums[rightIdx++];
+                       
+                }
+                mainIdx++;
           }
+         while(leftIdx<=mid){
+               temp[mainIdx] =nums[leftIdx];
+                leftIdx++;
+               mainIdx++;
+         }
+         while(rightIdx<=e){
+               temp[mainIdx] =nums[rightIdx];
+               rightIdx++;
+               mainIdx++;
+         }
+        for(int i=st;i<=e;i++) nums[i] = temp[i];
+       
     }
-    void mergeSort(vector<int>&ans,vector<int>&nums,int st, int e){
-           // base case
-           if(st>=e) return;
-           int mid = (st+e)/2;
-           // left subarray call
-           mergeSort(ans,nums,st,mid);
-           // right subarray call
-           mergeSort(ans,nums,mid+1,e);
-           // merge in ans array
-           merge(ans,nums,st,e);
+    void mergeSort(vector<int>& nums, int st, int e,vector<int>& temp){
+          // base condition
+          if(st>=e) return;
+          int mid = (st+e)/2;
+          mergeSort(nums,st,mid,temp);
+          mergeSort(nums,mid+1,e,temp);
+          merge(nums,st,e,temp);
     }
     vector<int> sortArray(vector<int>& nums) {
-           int size = nums.size();
-           vector<int>ans(size);
-           mergeSort(ans,nums,0,size-1);
-          return ans;
+          vector<int>temp(nums.size());
+          mergeSort(nums,0,nums.size()-1,temp);
+          return nums;
     }
 };
