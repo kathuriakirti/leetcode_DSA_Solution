7cc7b1d9e190ed8fd5d06c5627852eff02d7eaab
kathuriakirti <118164483+kathuriakirti@users.noreply.github.com>
2024-07-25 23:46:07 +0530

    Time: 86 ms (17.89%), Space: 70.6 MB (9.40%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
+    int maxSubArrayHelper(vector<int>& nums, int st, int e){
+          // base case
+          if(st==e) return nums[st];
+          int mid = st + ((e-st)>>1);
+          int maxLeftSub = maxSubArrayHelper(nums,st,mid);
+          int maxRightSub = maxSubArrayHelper(nums,mid+1,e);
+          int maxRightCross=INT_MIN, maxLeftCross=INT_MIN;
+          int leftCross=0,rightCross=0;
+          for(int i=mid;i>=st;i--){
+                 leftCross += nums[i];
+                 if(leftCross>maxLeftCross) maxLeftCross = leftCross;
+                     
+          }
+          for(int i=mid+1;i<=e;i++){
+                 rightCross+=nums[i];
+                 if(rightCross>maxRightCross) maxRightCross = rightCross;
+          }
+          int borderCross = maxLeftCross+maxRightCross;
+         return max(maxLeftSub,max(maxRightSub,borderCross));
+    }
     int maxSubArray(vector<int>& nums) {
-           int maxi = INT_MIN,sum=0;
-           for(int i=0;i<nums.size();i++){
-                   sum+=nums[i];
-                   maxi = max(sum,maxi);
-                   if(sum<0) sum=0;
-           }
-          return maxi;
+          return maxSubArrayHelper(nums,0,nums.size()-1);
     }
 };
